534723ed5037d1a2dd7e760a15b4bdadae7d3098
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-07 23:58:55 -0600

    poke api

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
 
 class Pokecard extends React.Component {
     render() {
