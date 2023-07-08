661c34987d7b0bd87b52aad3c43b18e91c116645
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-08 00:20:36 -0600

    render all pokemon from pokedex

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -18,7 +18,7 @@ class Pokedex extends Component {
         return (
             <div className="Pokedex">
                 <h1>Pokedex</h1>
-                {pokemon.map(p => <Pokecard />)}
+                {this.props.pokemon.map(p => <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />)}
             </div>
         )
     }
