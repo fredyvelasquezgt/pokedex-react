7f668ccdc95ec73e73732dc909631b80ba8f36a8
Fredy Velasquez <macacoloco696@gmail.com>
2023-07-07 23:52:12 -0600

    imports

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Pokecard extends React.Component {
     render() {
         return(
-            <h1>hello</h1>
+            <h1>POKECARD</h1>
         )
     }
 }
+
+export default Pokecard;
